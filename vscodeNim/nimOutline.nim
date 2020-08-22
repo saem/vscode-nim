3d181ec19cde23abf7f224dcc156f47d73f3e598
Saem Ghani <saemghani+github@gmail.com>
2020-08-22 14:24:32 -0700

    Removed console.log in nimOutline

--- vscodeNim/nimOutline.nim
+++ vscodeNim/nimOutline.nim
@@ -1,19 +1,16 @@
 import vscodeApi
 import tsNimExtApi
-import jsconsole
 
 proc provideWorkspaceSymbols*(
     query:cstring,
     token:VscodeCancellationToken
 ):Promise[seq[VscodeSymbolInformation]] =
-    console.log("here")
     return nimIndexer.findWorkspaceSymbols(query)
 
 proc provideDocumentSymbols*(
     doc:VscodeTextDocument,
     token:VscodeCancellationToken
 ):Promise[seq[VscodeSymbolInformation]] =
-    console.log("there")
     return nimIndexer.getFileSymbols(doc.filename, nimUtils.getDirtyFile(doc))
 
 var nimSymbolProvider* {.exportc.} = newJsObject()
